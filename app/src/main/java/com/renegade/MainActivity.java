c6c3cb22fabb00909be6d9cdb511804bd121ac3a
zafra1362 <zafra1362@gmail.com>
2021-05-17 20:53:00 +0200

    Initial commit

---
+++ app/src/main/java/com/renegade/MainActivity.java
@@ -0,0 +1,26 @@
+package com.renegade;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.renegade.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+        ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+    }
+}
